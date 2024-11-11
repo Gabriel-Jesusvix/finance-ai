a82287c501946a638e217cfa8609376f60510d3a
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-11-11 00:53:28 -0300

    feat: add ai generate

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,28 +1,32 @@
-import { auth } from "@clerk/nextjs/server";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
-import { DataTable } from "../_components/ui/data-table";
-import { db } from "../_lib/prisma";
-import { transactionColumns } from "./_columns";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
-export default async function TransactionsPage() {
+const TransactionsPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-
   const transactions = await db.transaction.findMany({
-    where: { userId },
+    where: {
+      userId,
+    },
   });
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
       <div className="space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
         <ScrollArea>
           <DataTable columns={transactionColumns} data={transactions} />
@@ -30,4 +34,6 @@ export default async function TransactionsPage() {
       </div>
     </>
   );
-}
+};
+
+export default TransactionsPage;
